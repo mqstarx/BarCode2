1d28e86cb1c38fd9e37e83bed1b18247631e9c61
mqsta <mqsta@DESKTOP-Q4CS7IC>
2018-02-26 22:48:31 +0300

    3

--- BarCode2/QrProcessor.cs
+++ BarCode2/QrProcessor.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataBase;
 
 namespace BarCode2
 {
@@ -30,120 +31,120 @@ namespace BarCode2
             qrCode = new QRCoder.QRCode(qr_data);
             bitmap = qrCode.GetGraphic(25);
             g.DrawImage(bitmap, p.X, p.Y, z, z);
-           
+
         }
-        private void SaveQrCode(string data, string filename,QRCoder.QRCodeGenerator.ECCLevel err_cor)
+        private void SaveQrCode(string data, string filename, QRCoder.QRCodeGenerator.ECCLevel err_cor)
         {
-            QRCoder.QRCodeData qr_data = qgen.CreateQrCode(data,err_cor);
+            QRCoder.QRCodeData qr_data = qgen.CreateQrCode(data, err_cor);
             qrCode = new QRCoder.QRCode(qr_data);
             bitmap = qrCode.GetGraphic(25);
             bitmap.Save(filename);
 
         }
-      /*  private void ParseQrPacket()
-        {
-            string res = "";
-            foreach (char c in QrPacket)
-            {
-                res += c;
-            }
-            if (res.IndexOf("FFX") != -1 && res.IndexOf("FXX") != -1 && res.Length > 9)
-            {
+        /*     private QrCodeData ParseQrPacket(List<char> QrPacket)
+             {
+                 string res = "";
+                 foreach (char c in QrPacket)
+                 {
+                     res += c;
+                 }
+                 QrCodeData _qrResult;
+                 //проверка наличия пакета
+                 if (res.IndexOf("FFX") != -1 && res.IndexOf("FXX") != -1 && res.Length > 9)
+                 {
+                     _qrResult = new QrCodeData();
 
 
+                    //получаем строку без заголовка
+                     string _packetWithoutHeader = res.Substring(res.IndexOf("FFX") + 6, int.Parse(res.Substring(res.IndexOf("FFX") + 3, 3)));
 
+                     string t_packet = datapacket;
+                     List<string> in_pac = new List<string>();
 
-                list_ident_data.Items.Clear();
-                ident_page.CreateGraphics().Clear(Color.White);
-                DrawQrCode(res, ident_page.CreateGraphics(), 200);
-                string datapacket = res.Substring(res.IndexOf("FFX") + 6, int.Parse(res.Substring(res.IndexOf("FFX") + 3, 3)));
+                     // парсинг входящих пакетов в состав основного пакета
 
-                string t_packet = datapacket;
-                List<string> in_pac = new List<string>();
+                     while (t_packet.IndexOf("FFY") != -1 && t_packet.IndexOf("FYY") != -1)
+                     {
+                         in_pac.Add(t_packet.Substring(t_packet.IndexOf("FFY") + 6, int.Parse(res.Substring(res.IndexOf("FFY") + 3, 3))));
+                         t_packet = t_packet.Substring(t_packet.IndexOf("FYY") + 3, t_packet.Length - t_packet.IndexOf("FYY") - 3);
+                     }
+                     if (in_pac.Count == 0)
+                     {
+                         foreach (PacketDataType p in DbList)
+                         {
+                             if (datapacket.IndexOf(p.TypeId) != -1)
+                             {
+                                 string d = datapacket.Substring(datapacket.IndexOf(p.TypeId) + 1, p.DataLen);
+                                 if (p.KeyValue.Count > 0)
+                                 {
+                                     foreach (KeyValuePair<string, string> kv in p.KeyValue)
+                                     {
+                                         if (kv.Key == d)
+                                             list_ident_data.Items.Add(p.DataDescr + ": " + kv.Value);
+                                     }
+                                 }
+                                 else
+                                 {
+                                     list_ident_data.Items.Add(p.DataDescr + ": " + d);
+                                 }
 
-                // парсинг входящих пакетов в состав основного пакета
+                             }
+                         }
+                         QrPacket.Clear();
+                     }
+                     else
+                     {
+                         string main_packet = datapacket.Substring(0, datapacket.IndexOf("FFY"));
+                         foreach (PacketDataType p in DbList)
+                         {
+                             if (main_packet.IndexOf(p.TypeId) != -1)
+                             {
+                                 string d = main_packet.Substring(main_packet.IndexOf(p.TypeId) + 1, p.DataLen);
+                                 if (p.KeyValue.Count > 0)
+                                 {
+                                     foreach (KeyValuePair<string, string> kv in p.KeyValue)
+                                     {
+                                         if (kv.Key == d)
+                                             list_ident_data.Items.Add(p.DataDescr + ": " + kv.Value);
+                                     }
+                                 }
+                                 else
+                                 {
+                                     list_ident_data.Items.Add(p.DataDescr + ": " + d);
+                                 }
 
-                while (t_packet.IndexOf("FFY") != -1 && t_packet.IndexOf("FYY") != -1)
-                {
-                    in_pac.Add(t_packet.Substring(t_packet.IndexOf("FFY") + 6, int.Parse(res.Substring(res.IndexOf("FFY") + 3, 3))));
-                    t_packet = t_packet.Substring(t_packet.IndexOf("FYY") + 3, t_packet.Length - t_packet.IndexOf("FYY") - 3);
-                }
-                if (in_pac.Count == 0)
-                {
-                    foreach (PacketDataType p in DbList)
-                    {
-                        if (datapacket.IndexOf(p.TypeId) != -1)
-                        {
-                            string d = datapacket.Substring(datapacket.IndexOf(p.TypeId) + 1, p.DataLen);
-                            if (p.KeyValue.Count > 0)
-                            {
-                                foreach (KeyValuePair<string, string> kv in p.KeyValue)
-                                {
-                                    if (kv.Key == d)
-                                        list_ident_data.Items.Add(p.DataDescr + ": " + kv.Value);
-                                }
-                            }
-                            else
-                            {
-                                list_ident_data.Items.Add(p.DataDescr + ": " + d);
-                            }
+                             }
+                         }
+                         foreach (string s in in_pac)
+                         {
 
-                        }
-                    }
-                    QrPacket.Clear();
-                }
-                else
-                {
-                    string main_packet = datapacket.Substring(0, datapacket.IndexOf("FFY"));
-                    foreach (PacketDataType p in DbList)
-                    {
-                        if (main_packet.IndexOf(p.TypeId) != -1)
-                        {
-                            string d = main_packet.Substring(main_packet.IndexOf(p.TypeId) + 1, p.DataLen);
-                            if (p.KeyValue.Count > 0)
-                            {
-                                foreach (KeyValuePair<string, string> kv in p.KeyValue)
-                                {
-                                    if (kv.Key == d)
-                                        list_ident_data.Items.Add(p.DataDescr + ": " + kv.Value);
-                                }
-                            }
-                            else
-                            {
-                                list_ident_data.Items.Add(p.DataDescr + ": " + d);
-                            }
+                             foreach (PacketDataType p in DbList)
+                             {
+                                 if (s.IndexOf(p.TypeId) != -1)
+                                 {
+                                     string d = s.Substring(s.IndexOf(p.TypeId) + 1, p.DataLen);
+                                     if (p.KeyValue.Count > 0)
+                                     {
+                                         foreach (KeyValuePair<string, string> kv in p.KeyValue)
+                                         {
+                                             if (kv.Key == d)
+                                                 list_ident_data.Items.Add(p.DataDescr + ": " + kv.Value);
+                                         }
+                                     }
+                                     else
+                                     {
+                                         list_ident_data.Items.Add(p.DataDescr + ": " + d);
+                                     }
 
-                        }
-                    }
-                    foreach (string s in in_pac)
-                    {
+                                 }
+                             }
+                         }
 
-                        foreach (PacketDataType p in DbList)
-                        {
-                            if (s.IndexOf(p.TypeId) != -1)
-                            {
-                                string d = s.Substring(s.IndexOf(p.TypeId) + 1, p.DataLen);
-                                if (p.KeyValue.Count > 0)
-                                {
-                                    foreach (KeyValuePair<string, string> kv in p.KeyValue)
-                                    {
-                                        if (kv.Key == d)
-                                            list_ident_data.Items.Add(p.DataDescr + ": " + kv.Value);
-                                    }
-                                }
-                                else
-                                {
-                                    list_ident_data.Items.Add(p.DataDescr + ": " + d);
-                                }
+                         QrPacket.Clear();
+                     }
 
-                            }
-                        }
-                    }
-
-                    QrPacket.Clear();
-                }
-
-            }
-        }*/
+                 }
+             }
+         */
     }
 }
